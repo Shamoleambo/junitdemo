5c1ab839712a3c34210fc07e716a658af72a4e4c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-25 12:41:02 -0300

    write a new test wher the null is the main star!

--- src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.luv2code.junitdemo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,16 @@ class DemoUtilsTest {
 
 		assertEquals(6, sut.add(3, 3), "3 + 3 must be 6");
 		assertNotEquals(6, sut.add(1, 1), "1 + 1 must be 2");
+	}
+
+	@Test
+	public void testNullAndNotNull() {
+		DemoUtils sut = new DemoUtils();
+
+		String isNull = null;
+		String notNull = "not null";
+
+		assertNull(sut.checkNull(isNull), "Object should be null");
+		assertNotNull(sut.checkNull(notNull), "Object should not be null");
 	}
 }
