17007998bdc884f516c8f84eae90b97f7a18dd0f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-29 14:34:12 -0300

    implement the test required code

---
+++ src/main/java/com/luv2code/junitdemo/tdd/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.luv2code.junitdemo.tdd;
+
+public class FizzBuzz {
+
+	public static String computer(int number) {
+		if (number % 3 == 0) {
+			return "Fizz";
+		}
+
+		return null;
+	}
+}
--- src/test/java/com/luv2code/junitdemo/tdd/FizzBuzzTest.java
+++ src/test/java/com/luv2code/junitdemo/tdd/FizzBuzzTest.java
@@ -1,6 +1,6 @@
 package com.luv2code.junitdemo.tdd;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -15,6 +15,7 @@ class FizzBuzzTest {
 	@DisplayName("Divisible by three")
 	@Order(1)
 	void testForDivisibleByThree() {
-		fail("fail");
+		String expected = "Fizz";
+		assertEquals(expected, FizzBuzz.computer(3), "Should return Fizz");
 	}
 }
