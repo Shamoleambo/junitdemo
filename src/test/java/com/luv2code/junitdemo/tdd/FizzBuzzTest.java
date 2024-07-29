090513ef36d26399fb43639559098242618bc30b
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-29 15:09:55 -0300

    do a bunch of stuff

--- src/test/java/com/luv2code/junitdemo/tdd/FizzBuzzTest.java
+++ src/test/java/com/luv2code/junitdemo/tdd/FizzBuzzTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
@@ -41,5 +43,13 @@ class FizzBuzzTest {
 	void testForNotDivisibleByThreeOrFive() {
 		String expected = "8";
 		assertEquals(expected, FizzBuzz.computer(8), "Should return the given number");
+	}
+
+	@ParameterizedTest(name = "value={0}, expected={1}")
+	@CsvFileSource(resources = "/test-data.csv")
+	@DisplayName("Testing with small data file")
+	@Order(5)
+	void testWithSmallDataFile(int value, String expected) {
+		assertEquals(expected, FizzBuzz.computer(value));
 	}
 }
